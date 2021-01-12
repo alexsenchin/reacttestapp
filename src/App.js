211df80b5ed642a93a75b5b6612e24ecb7a3c072
alexsenchin <osenchyn@icloud.com>
2021-01-12 15:12:47 +0200

    v2.02

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import style from './App.module.css';
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Messages/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
     return (
-        <div className={style.appWrapper}>
-            <Header/>
-            <Nav/>
-            <Profile/>
-        </div>
+        <BrowserRouter>
+            <div className={style.appWrapper}>
+                <Header/>
+                <Nav/>
+                <div>
+                    <Route path='/messages' render={ () => <Dialogs />}/>
+                    <Route path='/profile' render={ () => <Profile />}/>
+                    <Route path='/news'/>
+                    <Route path='/music'/>
+                    <Route path='/settings'/>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
